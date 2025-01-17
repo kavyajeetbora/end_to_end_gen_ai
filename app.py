59ae174cca33f21b73366ef3eb539ba679dfd280
= <=>
2025-01-17 21:33:13 +0530

    Add - streamlit frontend app

--- app.py
+++ app.py
@@ -6,18 +6,31 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
 
-def main():
-    
-    ## Load the 
-    url_loader = URLLoader(
-        urls = [
-            "https://talksport.com/topic/transfer-news/page",
-            "https://talksport.com/topic/transfer-news/page/2",
-            "https://talksport.com/topic/transfer-news/page/3"
-        ]
-    )
+import streamlit as st
 
+st.title("News Chatbot 🔎")
+st.subheader("This is a chatbot that can answer questions based on the news articles")
+
+st.sidebar.title("Enter the URLs")
+
+urls = []
+for i in range(3):
+    url = st.sidebar.text_input(f"URL {i+1}")
+    urls.append(url)
+
+button = st.sidebar.button("Search the URLs")
+
+query = st.text_input("Enter the question")
+
+progress_status = st.empty()
+answer = st.empty()
+    
+if len(urls)>0 and button and len(query) > 3:
+
+    ## Create the url loader
+    url_loader = URLLoader(urls = urls)
     documents = url_loader.load()
+    progress_status.text("URLs loaded successfully  ✅✅✅")
 
     ## Process the documents
     text_splitter = TextSplitter(
@@ -27,49 +40,50 @@ def main():
     )
 
     chunks = text_splitter.split()
+    progress_status.text(f"Documents processed successfully into {len(chunks)} chunks ✅✅✅")
 
     ## Once the chunking is done, Embedding
     embeddings = Embeddings()
     vector_store = embeddings.create_embeddings(chunks)
 
     if vector_store is not None:
-        print("Vector store created successfully")
+        progress_status.text("Vector store created successfully ✅✅✅")
+
+        ### HERE WE CREATE THE RAG PIPELINE
+
+        progress_status.text("Creating the RAG pipeline...⌛⌛⌛")
+        ## Search similar documents by query from retriever
+        retriever = vector_store.as_retriever(
+            search_type = "similarity",
+            search_kwargs = {"k": 3}
+        )
+
+        ## Create the prompt template
+        template = """Answer the question based only on the following context:
+        {context}
+
+        Question: {question}
+        """ 
+        prompt_template = ChatPromptTemplate.from_template(template=template)
+
+
+        ## Create the language model
+        llm = ChatOpenAI(model='gpt-4o-mini')
+
+        ## RAG Pipeline using LECL
+        chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | prompt_template
+            | llm
+            | StrOutputParser()
+        )
+        progress_status.text("RAG Pipeline created successfully ✅✅✅")
+
+        response = chain.invoke(query)
+
+        answer.text(response)
     else:
-        print("There was some problem in creating the vector store")
+        progress_status.text("There was some problem in creating the vector store")
 
 
-    ### HERE WE CREATE THE RAG PIPELINE
-
-    ## Search similar documents by query from retriever
-    retriever = vector_store.as_retriever(
-        search_type = "similarity",
-        search_kwargs = {"k": 3}
-    )
     
-    ## Create the prompt template
-    template = """Answer the question based only on the following context:
-    {context}
-
-    Question: {question}
-    """ 
-    prompt_template = ChatPromptTemplate.from_template(template=template)
-
-
-    ## Create the language model
-    llm = ChatOpenAI(model='gpt-4o-mini')
-
-    ## RAG Pipeline using LECL
-    chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt_template
-        | llm
-        | StrOutputParser()
-    )
-
-    query = "Who are manchester city targeting in the transfer market?"
-    response = chain.invoke(query)
-
-    print(response)
-
-if __name__ == "__main__":
-    main()
